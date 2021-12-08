15c5f7c9f8275e7a1188aa6c3f4b4c869c56274a
el <elena@madetech.com>
2021-12-08 13:50:51 +0000

    add appointment time domain

---
+++ HousingRepairsOnlineApi/Domain/AppointmentTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HACT.Dtos;
+
+namespace HousingRepairsOnlineApi.Domain
+{
+    public class AppointmentTime
+    {
+        public Reference Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
