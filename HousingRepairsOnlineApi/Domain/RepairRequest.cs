ce7cc3d9a7918a27c979f2637c58e46450a17858
el <elena@madetech.com>
2022-01-05 15:01:03 +0000

    add domain object and add item gateway

---
+++ HousingRepairsOnlineApi/Domain/RepairRequest.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace HousingRepairsOnlineApi.Domain
+{
+    public class RepairRequest
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        public string Postcode { get; set; }
+        public string SOR { get; set; }
+        public RepairAddress Address { get; set; }
+        public RepairLocation Location { get; set; }
+        public RepairProblem Problem { get; set; }
+        public RepairProblemBestDescription ProblemBestDescription { get; set; }
+        public string ContactPersonNumber { get; set; }
+        public RepairDescription Description { get; set; }
+        public RepairContactDetails ContactDetails { get; set; }
+        public RepairAvailability Time { get; set; }
+    }
+    public class RepairAddress
+    {
+        public string Display { get; set; }
+        public string LocationId { get; set; }
+    }
+    public class RepairLocation
+    {
+        public string value {get; set;}
+        public string display {get; set;}
+    }
+    public class RepairProblem
+    {
+        public string value {get; set;}
+        public string display {get; set;}
+    }
+    public class RepairProblemBestDescription
+    {
+        public string value {get; set;}
+        public string display {get; set;}
+    }
+    public class RepairDescription
+    {
+        public string photo {get;set;}
+        public string text {get;set;}
+    }
+    public class RepairContactDetails
+    {
+        public string type {get;set;}
+        public string value {get;set;}
+    }
+    public class RepairAvailability
+    {
+        public string value {get;set;}
+        public string display {get;set;}
+    }
+}
--- HousingRepairsOnlineApi/Gateways/CosmosGateway.cs
+++ HousingRepairsOnlineApi/Gateways/CosmosGateway.cs
@@ -1,18 +1,41 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Azure.Cosmos;
+using HousingRepairsOnlineApi.Domain;
 
 namespace HousingRepairsOnlineApi.Gateways
 {
     public class CosmosGateway : ICosmosGateway
     {
-        private CosmosContainer cosmosContainer;
+        private static CosmosContainer cosmosContainer;
 
         public CosmosGateway(CosmosContainer cosmosContainer)
         {
             cosmosContainer = cosmosContainer;
         }
 
+        /// <summary>
+        /// Add RepairRequest items to the container
+        /// </summary>
+        private static async Task AddItemToContainerAsync(RepairRequest repairRequest)
+        {
+            CosmosContainer container = cosmosContainer;
+            try
+            {
+                // Read the item to see if it exists.
+                ItemResponse<RepairRequest> itemResponse = await container.ReadItemAsync<RepairRequest>(repairRequest.Id, new PartitionKey(repairRequest.Id));
+                Console.WriteLine("Item in database with id: {0} already exists\n", itemResponse.Value.Id);
+            }
+            catch(CosmosException ex) when (ex.Status == (int)HttpStatusCode.NotFound)
+            {
+                // Create an item in the container. Note we provide the value of the partition key for this item, which is ID
+                ItemResponse<RepairRequest> itemResponse = await container.CreateItemAsync<RepairRequest>(repairRequest, new PartitionKey(repairRequest.Id));
+
+                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse.
+                Console.WriteLine("Created item in database with id: {0}\n", itemResponse.Value.Id);
+            }
+        }
     }
 
 }
