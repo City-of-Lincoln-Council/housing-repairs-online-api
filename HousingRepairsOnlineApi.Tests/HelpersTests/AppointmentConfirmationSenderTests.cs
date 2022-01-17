fc7f04fdff3668d408f199de13202a9ac12bb9c5
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2022-01-17 15:37:47 +0000

    Update HousingRepairsOnlineApi.Tests/HelpersTests/AppointmentConfirmationSenderTests.cs
    Co-authored-by: Bhavesh-Patel <Bhavesh-Patel@users.noreply.github.com>

--- HousingRepairsOnlineApi.Tests/HelpersTests/AppointmentConfirmationSenderTests.cs
+++ HousingRepairsOnlineApi.Tests/HelpersTests/AppointmentConfirmationSenderTests.cs
@@ -22,7 +22,7 @@ namespace HousingRepairsOnlineApi.Tests.HelpersTests
         [Fact]
         public void GivenEmailContact_WhenExecute_ThenSendAppointmentConfirmationEmailUseCaseIsCalled()
         {
-            var repairRequest = new RepairRequest { ContactDetails = new RepairContactDetails { Type = "email", Value = "abc@defg.hij" }, Time = new RepairAvailability() { Display = "some time" } };
+            var repairRequest = new RepairRequest { ContactDetails = new RepairContactDetails { Type = HousingRepairsOnlineApi.Helpers.Constants.Email, Value = "abc@defg.hij" }, Time = new RepairAvailability() { Display = "some time" } };
             systemUnderTest.Execute(repairRequest, "id");
             sendAppointmentConfirmationEmailUseCaseMock.Verify(x => x.Execute(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
             sendAppointmentConfirmationSmsUseCaseMock.Verify(x => x.Execute(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
