999f9600963e8d489308a47fd2acffe4fb6aa6ed
maysakanoni <maysa@madetech.com>
2022-01-20 16:02:03 +0000

    move if statement outside task run method

--- HousingRepairsOnlineApi/Helpers/InternalEmailSender.cs
+++ HousingRepairsOnlineApi/Helpers/InternalEmailSender.cs
@@ -19,14 +19,13 @@ namespace HousingRepairsOnlineApi.Helpers
         public async Task Execute(Repair repair)
         {
             var imageLink = "None";
-
-            await Task.Run(() =>
+            if (!String.IsNullOrEmpty(repair.Description?.PhotoUrl))
             {
-                if (!String.IsNullOrEmpty(repair.Description?.PhotoUrl))
+                await Task.Run(() =>
                 {
                     imageLink = retrieveImageLinkUseCase.Execute(repair.Description?.PhotoUrl);
-                }
-            });
+                });
+            }
             sendInternalEmailUseCase.Execute(repair.Id, repair.Address.LocationId, repair.Address.Display, repair.SOR, repair.Description.Text, repair.ContactDetails?.Value, imageLink);
         }
     }
