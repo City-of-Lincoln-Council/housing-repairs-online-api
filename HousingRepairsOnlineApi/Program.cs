0810a133f0a12a0d4a2652d86defd5b4dd548e7a
el <elena@madetech.com>
2022-02-09 11:14:27 +0000

    add sentry

--- HousingRepairsOnlineApi/Program.cs
+++ HousingRepairsOnlineApi/Program.cs
@@ -20,6 +20,15 @@ namespace HousingRepairsOnlineApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSentry(o =>
+                    {
+                        o.Dsn =  Environment.GetEnvironmentVariable("SENTRY_DNS");
+                        // When configuring for the first time, to see what the SDK is doing:
+                        o.Debug = true;
+                        // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                        // We recommend adjusting this value in production.
+                        o.TracesSampleRate = 1.0;
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
