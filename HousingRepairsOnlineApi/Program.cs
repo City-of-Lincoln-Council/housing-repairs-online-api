26b0e63681cc1467e0de2175e410583662442f7e
el <elena@madetech.com>
2022-02-09 12:22:31 +0000

    format

--- HousingRepairsOnlineApi/Program.cs
+++ HousingRepairsOnlineApi/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HousingRepairsOnlineApi
 {
@@ -22,7 +17,7 @@ namespace HousingRepairsOnlineApi
                 {
                     webBuilder.UseSentry(o =>
                     {
-                        o.Dsn =  Environment.GetEnvironmentVariable("SENTRY_DNS");
+                        o.Dsn = Environment.GetEnvironmentVariable("SENTRY_DNS");
                         // When configuring for the first time, to see what the SDK is doing:
                         o.Debug = true;
                         // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
