7ed9e1a0423e4f7c1aa3c94f0ef8c2d056b801c7
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-03-25 17:19:25 +0000

    Adding serverless deployment via CI

---
+++ HousingRepairsOnlineApi/LambdaEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HousingRepairsOnlineApi
+{
+    [ExcludeFromCodeCoverage]
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();
+        }
+    }
+}
