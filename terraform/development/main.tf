03299497b6f257cf9d3a6352b9d914cf7190c8f4
Bhavesh-Patel <Bhavesh-Patel@users.noreply.github.com>
2022-04-11 14:16:42 +0100

    Adding Terraform (#3)
    * Adding Terraform
    
    Co-authored-by: jamiejordanmt <jamie.jordan@hackney.gov.uk>

---
+++ terraform/development/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 2.0"
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+locals {
+  application_name = "housing-repairs-online-api" # The name to use for your application
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-housing-development"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/housing-repairs-online-api/state"
+  }
+}
