8bb289c614c2d2042d99319124a3345c46d62526
Dave Dean <david.dean@hackney.gov.uk>
2022-05-19 16:44:49 +0100

    Mapper and tests

--- HousingRepairsOnlineApi.Tests/MappersTests/MapRepairsOnlineToRepairsHubTests.cs
+++ HousingRepairsOnlineApi.Tests/MappersTests/MapRepairsOnlineToRepairsHubTests.cs
@@ -3,6 +3,8 @@ using HousingRepairsOnlineApi.Mappers;
 using Xunit;
 using FluentAssertions;
 using System;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace HousingRepairsOnlineApi.Tests.MappersTests
 {
@@ -14,37 +16,205 @@ namespace HousingRepairsOnlineApi.Tests.MappersTests
             sut = new MapRepairsOnlineToRepairsHub();
         }
 
-        [Fact]
-        public async void DescriptionIsNullOrEmpty()
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void DescriptionIsNotNullOrEmpty(string desc)
         {
             // Arrange
             var repairRequest = new RepairRequest
             {
                 Description = new RepairDescriptionRequest
                 {
-                    Text = "Sample repair description"
+                    Text = desc
                 }
             };
 
-            var repair = new Repair();
+            var repair = GenerateValidRepair();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => sut.Map(repairRequest, repair));
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("55000")]
+        [InlineData("19999999")]
+        [InlineData("30000001")]
+        public void ReferenceIsNotEmptyAndIsGreaterThan20Mil(string id)
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+
+            var repair = GenerateValidRepair();
+            repair.Id = id;
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => sut.Map(repairRequest, repair));
+        }
+
+        [Fact]
+        public void ReferenceIsNotNull()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+
+            var repair = new Repair
+            {
+                Id = null
+            };
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => sut.Map(repairRequest, repair));
+        }
+
+        [Fact]
+        public void PriorityIsSetCorrectly()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
 
             // Act
             var result = sut.Map(repairRequest, repair);
 
             // Assert
-            result.DescriptionOfWork.Should().NotBeNullOrEmpty();
+            result.Priority.PriorityCode.Should().Be(4);
+            result.Priority.PriorityDescription.Should().Be("5 [N] NORMAL");
+            result.Priority.NumberOfDays.Should().Be(21);
         }
-       
 
-        // Reference can't' be null or empty and must be more than 20,000,000
-        // Check that the priority, workclass, budget code, multi-trade are set
-        // Instructed by is always "Hackney Housing" -> could get a new value for this?
-        // Organisation always set to H01
-        // AssignedToPrimary is always "HH General Building Repai"
-        // Site has a single entry, has the address/postcode/propref
+        [Fact]
+        public void FixedAttributesSetCorrectly()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
+
+            // Act
+            var result = sut.Map(repairRequest, repair);
+
+            // Assert
+            result.WorkClass.WorkClassCode.Should().Be(0);
+            result.BudgetCode.Id.Should().Be("8");
+            result.MultiTradeWorkOrder.Should().BeFalse();
+        }
+
+        [Fact]
+        public void OrganisationSetCorrectly()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
+
+            // Act
+            var result = sut.Map(repairRequest, repair);
+
+            // Assert
+            result.InstructedBy.Name.Should().Be("Hackney Housing");
+            result.AssignedToPrimary.Name.Should().Be("HH General Building Repai");
+        }
+
+        [Fact]
+        public void AddressDataIsSetCorrectly()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
+
+            // Act
+            var result = sut.Map(repairRequest, repair);
+
+            // Assert
+            var prop = result.Site.Property.Single();
+
+            prop.PropertyReference.Should().Be("00003277");
+            prop.Address.PostalCode.Should().Be("N4 2FL");
+        }
+
+        [Fact]
+        public void CustomerNameIsBlank()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
+
+            // Act
+            var result = sut.Map(repairRequest, repair);
+
+            // Assert
+            result.Customer.Name.Should().Be("");
+            result.Customer.Person.Name.Full.Should().Be("");
+        }
+
+        [Fact]
+        public void CustomerPhoneIsCorrect()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
+
+            // Act
+            var result = sut.Map(repairRequest, repair);
+
+            // Assert
+            var comm = result.Customer.Person.Communication.Single();
+            comm.Channel.Medium.Should().Be("20");
+            comm.Channel.Code.Should().Be("60");
+            comm.Value.Should().Be("07720340340");
+        }
+
+        [Fact]
+        public void WorkElementIsCorrect()
+        {
+            // Arrange
+            var repairRequest = GenerateValidRequest();
+            var repair = GenerateValidRepair();
+
+            // Act
+            var result = sut.Map(repairRequest, repair);
+
+            // Assert
+            var workElement = result.WorkElement.Single();
+
+            var trade = workElement.Trade.Single();
+            trade.Code.Should().Be("SP");
+            trade.CustomCode.Should().Be("EL");
+            trade.CustomName.Should().Be("Electrical - EL");
+
+            var sor = workElement.RateScheduleItem.Single();
+            sor.CustomCode.Should().Be("20110200");
+            sor.CustomName.Should().Be("EMERGENCY LIGHT TEST DWELL");
+        }
+
+        private RepairRequest GenerateValidRequest()
+        {
+            return new RepairRequest
+            {
+                Description = new RepairDescriptionRequest
+                {
+                    Text = "Valid description"
+                },
+                Address = new RepairAddress
+                {
+                    LocationId = "00003277",
+                    Display = "Sample location 1"
+                },
+                Postcode = "N4 2FL",
+                ContactPersonNumber = "07720340340"
+            };
+        }
+
+        private Repair GenerateValidRepair()
+        {
+            return new Repair
+            {
+                Id = "20000000",
+                SOR = "20110200"
+            };
+        }
+
         // Work element is not null, has exactly one rate schedule item, has a trade
-        // DateRaised and DateReported both set to today
-        // Customer has a communication channel with one phone number
-        // Customer's name has to be an empty string
     }
 }
--- HousingRepairsOnlineApi/Mappers/MapRepairsOnlineToRepairsHub.cs
+++ HousingRepairsOnlineApi/Mappers/MapRepairsOnlineToRepairsHub.cs
@@ -1,15 +1,118 @@
 ﻿using HousingRepairsOnlineApi.Domain;
 using HousingRepairsOnlineApi.Domain.Boundaries;
+using Ardalis.GuardClauses;
+using System.Collections.Generic;
+using Outbound = HousingRepairsOnlineApi.Domain.Boundaries.RepairsHub;
 
 namespace HousingRepairsOnlineApi.Mappers;
 
 public class MapRepairsOnlineToRepairsHub : IMapRepairsOnlineToRepairsHub
 {
-    public RepairsHubCreationRequest Map(RepairRequest repairRequest, Repair result)
+    public RepairsHubCreationRequest Map(RepairRequest repairRequest, Repair repair)
     {
+        Guard.Against.InvalidInput(repairRequest.Description.Text, nameof(repairRequest.Description.Text), d => !string.IsNullOrEmpty(d));
+        Guard.Against.OutOfRange(int.Parse(repair.Id), nameof(repair.Id), 20000000, 30000000);
+
+
         return new RepairsHubCreationRequest
         {
-            DescriptionOfWork = repairRequest.Description.Text
+            DescriptionOfWork = repairRequest.Description.Text,
+            Priority = new Outbound.Priority
+            {
+                PriorityCode = 4,
+                PriorityDescription = "5 [N] NORMAL",
+                NumberOfDays = 21
+            },
+            WorkClass = new Outbound.WorkClass
+            {
+                WorkClassCode = 0
+            },
+            BudgetCode = new Outbound.BudgetCode
+            {
+                Id = "8"
+            },
+            MultiTradeWorkOrder = false,
+            Site = new Outbound.Site
+            {
+                Property = new List<Outbound.Property>
+                {
+                    new Outbound.Property
+                    {
+                        PropertyReference = repairRequest.Address.LocationId,
+                        Address = new Outbound.Address
+                        {
+                            PostalCode = repairRequest.Postcode
+                        }
+                    }
+                }
+            },
+            AssignedToPrimary = new Outbound.AssignedToPrimary
+            {
+                Name = "HH General Building Repai",
+                Organization = new Outbound.Organization
+                {
+                    Reference = new List<Outbound.Reference>
+                    {
+                        new Outbound.Reference
+                        {
+                            Id = "H01"
+                        }
+                    }
+                }
+            },
+            InstructedBy = new Outbound.InstructedBy
+            {
+                Name = "Hackney Housing"
+            },
+            Customer = new Outbound.Customer
+            {
+                Name = string.Empty,
+                Person = new Outbound.Person
+                {
+                    Name = new Outbound.Name
+                    {
+                        Full = ""
+                    },
+                    Communication = new List<Outbound.Communication>
+                    {
+                        new Outbound.Communication
+                        {
+                           Value = repairRequest.ContactPersonNumber,
+                           Channel = new Outbound.Channel
+                           {
+                               Code = "60",
+                               Medium = "20"
+                           }
+                        }
+                    }
+                }
+            },
+            WorkElement = new List<Outbound.WorkElement>
+            {
+                new Outbound.WorkElement
+                {
+                    Trade = new List<Outbound.Trade>
+                    {
+                        new Outbound.Trade
+                        {
+                            CustomCode = "TBC",
+                            CustomName = "TBC"
+                        }
+                    },
+                    RateScheduleItem = new List<Outbound.RateScheduleItem>
+                    {
+                        new Outbound.RateScheduleItem
+                        {
+                            Quantity = new Outbound.Quantity
+                            {
+                                Amount = { 1 }
+                            },
+                            CustomCode = repair.SOR,
+                            CustomName = "TBC"
+                        }
+                    }
+                }
+            }
         };
     }
 }
